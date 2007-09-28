a8d1d747fb0ce3de0190d2897dd370dc45f4748b
Thomas Langewouters <thomas.langewouters@thouters.be>
2007-09-28 18:48:47 +0200

    Imported ical code and model developed outside git.

---
+++ ical.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+
+# Ical classes
+# (c) 2007 Thomas Langewouters
+
+# http://www.ietf.org/rfc/rfc2445.txt
+
+import datetime, socket, string
+
+class IcalEvent:
+	""" An event, use constructor or populate with data,
+		retrieve it's repr() to get data 
+
+		@dtstamp, dtstart, dtend: datetime.Datetime instances
+		@description,summary: string instances
+		@location: obj w/name
+		@organizer: obj w/name email
+		@reqpart: [<obj w/name email>] """
+
+	interface = ["dtstamp","dtstart","dtend","description",
+				"summary","location","organizer","reqpart"]
+
+	template = """BEGIN:VEVENT
+UID:$idhash@$hostname
+ORGANIZER;CN=$orgname:MAILTO:$orgemail
+$reqpart
+DTSTAMP:$dtstamp
+DTSTART;TZID="Brussel, Kopenhagen, Madrid, Parijs":$dtstart
+DTEND;TZID="Brussel, Kopenhagen, Madrid, Parijs":$dtend
+SEQUENCE:0
+TRANSP:OPAQUE
+SUMMARY;LANGUAGE=nl;ENCODING=QUOTED-PRINTABLE:$summary 
+SUMMARY:$summary 
+LOCATION:$location
+DESCRIPTION;LANGUAGE=nl;ENCODING=QUOTED-PRINTABLE:$description
+DESCRIPTION:$description
+CATEGORIES:School
+CLASS:PUBLIC
+STATUS:CONFIRMED
+END:VEVENT
+"""
+	def __init__(self,m = None):
+		""" Pass a model instance to this function if you like """
+		if m: self.link(m)
+
+	def link(self,m):
+		""" Link instance attributes to model """
+		for i in self.interface:
+			setattr(self,i,getattr(m,i))
+
+	def __repr__(self):
+		""" display text representation of instance attributes """
+		# verify all requirements are met
+		if None in [getattr(self, x,None) for x in self.interface]:
+			return "<incomplete IcalEvent>"
+		# Convert timestamps to UTC ascii repr 
+		# (thus not having to send along timezone information...)
+		dtstamp = self.dtstamp.strftime("%Y%m%dT%H%M%SZ"),
+		dtstart = self.dtstart.strftime("%Y%m%dT%H%M%S"),
+		dtend   = self.dtend.strftime("%Y%m%dT%H%M%S"),
+		# create new strings with result of retrieval of said attrs.
+		description =	 "%s" % self.description
+		summary =		 "%s" % self.summary
+		location =		 "%s" % self.location.name
+		# build a string with participant lines glued with newlines
+		reqpart = "\n".join("ATTENDEE;ROLE=REQ-PARTICIPANT;CN=%s:MAILTO:%s" % \
+							(p.name, p.email) for p in self.reqpart	)
+		orgname =	"%s" % self.organizer.name
+		orgemail =	"%s" % self.organizer.email
+		# create a hash of key 
+		hostname = socket.getfqdn()
+		idhash = hash((dtstart, dtend, description, summary, location))
+		# substitute local variables in the template string where possible
+		return string.Template(self.template).substitute(locals())
+
+class IcalFile:
+	mime = "Content-type: text/calendar"
+	timezone = """BEGIN:VTIMEZONE
+TZID:Brussel\, Kopenhagen\, Madrid\, Parijs
+BEGIN:STANDARD
+DTSTART:20001029T030000
+RRULE:FREQ=YEARLY;INTERVAL=1;BYDAY=-1SU;BYMONTH=10
+TZNAME:Romance (standaardtijd)
+TZOFFSETFROM:+0200
+TZOFFSETTO:+0100
+END:STANDARD
+BEGIN:DAYLIGHT
+DTSTART:20000326T020000
+RRULE:FREQ=YEARLY;INTERVAL=1;BYDAY=-1SU;BYMONTH=3
+TZNAME:Romance (zomertijd)
+TZOFFSETFROM:+0100
+TZOFFSETTO:+0200
+END:DAYLIGHT
+END:VTIMEZONE
+"""
+	def __init__(self,list = None):
+		self.list = list
+	def __repr__(self):
+		if not self.list:
+			return "<Empty Ical File>"
+		out = "BEGIN:VCALENDAR\n"
+		out += "PRODID:-//TL//PyIcalCode//EN\n"
+		out += "VERSION:2.0\n"
+		out += "METHOD:REQUEST\n"
+		out += self.timezone
+		out += "".join(map(repr,map(IcalEvent,self.list)))
+		out += "END:VCALENDAR"
+		return out
+
+class testEvent:
+	""" example data model, for test purposes """
+	class exampleparticipant: 
+		""" example participant model, pass it a number """
+		def __init__(self,n):
+			self.name = "participant%i" % n
+			self.email = "participant%i@email.com" % n
+	class o: pass
+
+	def __init__(self,hour):
+		""" Create a test event at april first 2007, at the hour given"""
+		# our dummy organizer
+		self.organizer = self.o()
+		self.organizer.name = "Johndoe"
+		self.organizer.email = "Johndoe@email.com"
+		# ten dummy participants
+		self.reqpart = map(self.exampleparticipant,range(10))
+		# modstamp today
+		self.dtstamp = datetime.datetime.today()
+		# april first, hour given
+		self.dtstart = datetime.datetime(2007,4,1,hour)
+		# event takes one hour
+		self.dtend = self.dtstart + datetime.timedelta(hours=1)
+		self.description = "Example event"
+		self.summary = "Summarytext goes here"
+		self.location = self.o()
+		self.location.name = "Conference room"
+
+if __name__ == '__main__':
+	# output an icalfile with two testevents, one at 10am and one at 12
+	print IcalFile([IcalEvent(testEvent(10)),IcalEvent(testEvent(12))])
--- model.py
+++ model.py
@@ -1,32 +1,79 @@
+from sqlobject import *
+import datetime
 
-from sqlobject import SQLObject, StringCol, IntCol, DateCol, PickleCol,ForeignKey,MultipleJoin,RelatedJoin,DatabaseIndex
+__connection__ = "sqlite:/:memory:"
 
-__connection__ = "sqlite://:memory:"
+# Calculate first day of the current academic year.
+first = datetime.date.today().replace(month=9, day=1)
+# 1sept dit jaar reeds voorbij is, academiejaar begon vorig jaar
+if datetime.date.today() < first:
+	first= first.replace(year = first.year - 1)	
+# 3 weken na de eerste werkdag van september (FIXME)
+first = first + datetime.timedelta(days = 21 + 7 - first.weekday()) 
 
-class Room(SQLObject):
-	roomid			= StringCol(length=4,alternateID=True)
+class Klas(SQLObject):
+	name = StringCol()
+	groups = MultipleJoin('Group')
+	courses = MultipleJoin('Course')
 
-class Lecture(SQLObject):
-	course			= ForeignKey('Course')
-	location		= ForeignKey('Room')
-	presidents		= RelatedJoin('Professor')
-	audience		= RelatedJoin('StudyGroup')
-	SessionNumber	= IntCol()	
-	#Duration		= Timedelta() (minutes?)
-	#date, begintime, week, weekday
+class Group(SQLObject):
+	part = IntCol()
+	klas = ForeignKey("Klas")
+	lectures = MultipleJoin('Lecture')
 
 class Professor(SQLObject):
-	pass
-
-class StudyGroup(SQLObject):
-	pass
+	name = StringCol()
+	courses = MultipleJoin('Course')
+	lectures = MultipleJoin('Lecture')
 	
-
 class Course(SQLObject):
-	pass
-	
-class Klas(SQLObject):
-	pass
+	name = StringCol()
+	enrolled = MultipleJoin('Klas')
+	staff = MultipleJoin('Professor')
+	lectures = MultipleJoin('Lecture')
 
-class Study(SQLObject):
-	pass
+class Lecture(SQLObject):
+	# General
+	course = ForeignKey('Course')
+	audience = MultipleJoin('Group')
+	room = ForeignKey('Room')
+	# Raw data
+	# begin, span = minutes.
+	week = IntCol()
+	weekday = IntCol()
+	begin = IntCol()
+	span = IntCol()
+	# Buffered
+	dtstamp = DateTimeCol(default=None)
+	dtstart = DateTimeCol(default=None)
+	dtend = DateTimeCol(default=None)
+	def SetUpdated(self):
+		""" Update timestamps and mtime """
+		#FIXME, doe dit in toewijzing _set_mtime
+		#hash hier ook in verwerken???
+		self.mtime = datetime.datetime.now()
+		day = first + datetime.timedelta(days = (int(self.weekday) - 1), weeks = int(self.week) - 1)
+		self.dtstart = datetime.datetime.combine(day,datetime.time(self.begin/60,self.begin%60))
+		self.dtend = self.dtstart + datetime.timedelta(minutes = self.span)
+
+	# iCal-glue code
+	def _get_description(self):
+		""" Print much info, like attendents, profs,... """
+		pass
+	def _get_summary(self):
+		return self.course.name
+	def _get_location(self):
+		return self.room
+	def _get_reqpart(self):
+		return list(self.audience) + list(self.course.staff)
+	def _get_organizer(self):
+		class organizer: pass
+		x = organizer()
+		x.name = "put organizer here"
+		x.email = "email@example.com"
+		return x
+		
+class Room(SQLObject):
+	name = StringCol()
+	lectures = MultipleJoin('Lecture')
+
