cf3898a5905b143ecf63b65613ee1e0c7885e11a
Thomas Langewouters <thomas.langewouters@thouters.be>
2007-10-09 23:19:16 +0200

    cleanup of data structures and webretrieval code

--- parsers.py
+++ parsers.py
@@ -55,91 +55,7 @@ class LinkClass:
 
 class ParseError(Exception): pass
 
-class WebsiteSource:
-	""" This class should be a repository you can ask for timetables """
-	tables = []	
-	departments = [('IW','iw'),('Tech','tech')] #['ArchitectuurSLB','ArchitectuurSLG']
-	url= "http://sws.wenk.be/get_timetable.php"
-	def UpdateCandidates(self):
-		""" Build a list of timetables using data from sws.wenk.be """
-		source = "http://sws.wenk.be/js/filter_%s.js"
-		weekentry = r"""^\s+AddWeeks\s*\(\s*"(\d+)"\s*,\s*\"()\"\s*,\s*form.elements\["weeks"\]\);\s*$"""
-
-		def JSarrayToList(source, varname, depth):
-			""" obtain a javascript array defined with js sourcecode """
-			ret = {}
-			# Obtain a list of array store operations
-			arrayentry = r"""%s\s*\[\s*(\d+)\s*\]\s*\[\s*(\d+)\s*\]\s*=\s*"(.*)"\s*;"""
-			regex = re.compile(arrayentry % varname)
-			ops = re.findall(regex, source)
-			# Ensurance
-			if len(ops) == 0:
-				raise Exception("Parse error")
-			if len(ops) % depth > 0:
-				raise Exception("Parsed invalid number of entries")
-			for x in ops:
-				n = int(x[0])
-				if not n in ret.keys():
-					ret[n] = [None, None, None]
-				ret[n][int(x[1])] = x[2]
-			return ret.values()
-
-		self.tables = []
-		for department in self.departments:
-			remotefile = urllib.urlopen(source % department[1])
-			contents = remotefile.read()
-			remotefile.close()
-			
-			for i in JSarrayToList(contents, 'roomarray', 3):
-				self.tables.append(RoomTable(*[i[0],i[2],department[0]]))
-			for i in JSarrayToList(contents, 'staffarray', 3):
-				self.tables.append(ProfessorTable(*[i[0],i[2],department[0]]))
-			for i in JSarrayToList(contents, 'studentsetarray', 3):
-				self.tables.append(KlasTable(*[i[0],i[2],department[0]]))
-
-	def getTable(self,id):
-		return self.getTables([id])[0]
-
-	def getTables(self,tlist):
-		""" Fetch tables """
-		if not tlist:
-			raise Exception("no tables given")
-		# tables need to be fetched one department at a time?
-		#print [t.name for t in tlist]
-		for dept in self.departments:
-			print "any of these? ",dept
-			tables = filter(lambda t: t.department == dept[0],tlist)
-			print map(lambda x: x.department,tlist)
-			#testing with one at a time for now
-			table = tables[0]
-			getdict = {	"identifier[]": table.id,
-						"weeks":"1-13", "type": table.objectclass,
-						"filter":"(None)", "dept": table.department}
-			# do we have to close af file object?
-			remotefile = urllib.urlopen(self.url,urllib.urlencode(getdict))
-			source = remotefile.read()
-			remotefile.close()
-			# build tree
-			soup = BeautifulSoup(source)
-			# Iterate through all TIMETABLES in the HTML
-			for ttheader in soup.html.body.findAll("table",{"class":"header-border-args"}):
-				id = ttheader.find('span',{'class':'header-2-1-1'}).string.strip()
-				try:
-					x = filter(lambda x: x.name == id, tables)[0]
-				except KeyError:
-					raise ParseError, "Returned table not requested"
-				x.Parse(ttheader)
-		return tlist
-
-	def byName(self,id):
-		""" return an iterator over lectures.
-			@id the name of the klas/prof/room """
-		if not self.tables:
-			self.UpdateCandidates()
-		x = filter(lambda x: x.name == id, self.tables)
-		return x
-
-class TableParser:
+class LectureTable:
 	""" Fetches, parses a timetable from the internet,
 		provides an iterator over events				"""
 	Lectures = []
@@ -164,10 +80,10 @@ class TableParser:
 	class Lecture:
 		""" Helper class """
 		def __init__(self,klas,title,prof,room,start,duration,weekday,week):
-			self.klas = TableParser.Klas.link(unicode(klas),self)
-			self.course = TableParser.Course.link(unicode(title),self)
-			self.professor = TableParser.Professor.link(unicode(prof),self)
-			self.room = TableParser.Room.link(unicode(room),self)
+			self.klas = LectureTable.Klas.link(unicode(klas),self)
+			self.course = LectureTable.Course.link(unicode(title),self)
+			self.professor = LectureTable.Professor.link(unicode(prof),self)
+			self.room = LectureTable.Room.link(unicode(room),self)
 			self.start = start
 			self.duration = duration
 			self.weekday = weekday
@@ -187,6 +103,7 @@ class TableParser:
 			day_from_col = []
 			# GENERIC HEADER - has some crucial data
 			ttype = ttheader.find('span',{'class':'header-2-1-0'}).string.strip()[:-1]
+			# hier type herkennen en magie doen
 			klas = ttheader.find('span',{'class':'header-2-1-1'}).string.strip()
 			wspan = [ttheader.find('span',{'class':'header-3-0-1'}).string.strip(),ttheader.find('span',{'class':'header-3-0-3'}).string.strip()]
 			wspan = map(int,wspan)
@@ -213,17 +130,117 @@ class TableParser:
 						weekday = day_from_col[nth]
 						# rowspan -> 30 minute blocks
 						duration = timedelta(minutes=int(nthcell['rowspan']) * 30)
+						# student:   title -- weeks -- room -- prof
+						# docent:    title -- klassen -- weeks -- lokalen
+						# room:      title -- klassen -- weeks -- docent
 						(title,weeks,room,prof) = map(lambda x: x.string.strip(), nthcell.findAll('td'))
 						for week in WeekNotation2Array(weeks):
 							self.Lectures.append(self.Lecture(klas,title,prof,room,start,duration,weekday,week))
 			self.hasdata = True
 
-class ProfessorTable(TableParser):
-	objectclass = "staff"
-class RoomTable(TableParser):
-	objectclass = "room"
-class KlasTable(TableParser):
-	objectclass = "student"
+class ProfessorTable(LectureTable):
+	typeid = "staff"
+	clistfilter = "staffarray"
+class RoomTable(LectureTable):
+	typeid = "room"
+	clistfilter = "roomarray"
+class KlasTable(LectureTable):
+	typeid = "student"
+	clistfilter = "studentsetarray"
+
+class WebsiteSource:
+	""" This class should be a repository you can ask for timetables """
+	departments = ['IW','Tech']
+	ttypes = [ProfessorTable, RoomTable, KlasTable]
+	geturl= "http://sws.wenk.be/get_timetable.php"
+	listurl = "http://sws.wenk.be/js/filter_%s.js"
+	tables = []	
+
+	def UpdateCandidates(self):
+		""" Build a list of available timetables using data from sws.wenk.be """
+
+		def JSarrayToList(source, varname, depth):
+			""" obtain a javascript array defined with js sourcecode """
+			ret = {}
+			# Obtain a list of array store operations
+			arrayentry = r"""%s\s*\[\s*(\d+)\s*\]\s*\[\s*(\d+)\s*\]\s*=\s*"(.*)"\s*;"""
+			regex = re.compile(arrayentry % varname)
+			ops = re.findall(regex, source)
+			# Ensurance
+			if len(ops) == 0:
+				raise Exception("Parse error")
+			if len(ops) % depth > 0:
+				raise Exception("Parsed invalid number of entries")
+			for x in ops:
+				n = int(x[0])
+				if not n in ret.keys():
+					ret[n] = [None, None, None]
+				ret[n][int(x[1])] = x[2]
+			return ret.values()
+
+		self.tables = []
+		for department in self.departments:
+			# each department has its own list with staff, groups,...
+			# from this, build a list of instances of tabletypes
+			remotefile = urllib.urlopen(self.listurl % department.lower())
+			contents = remotefile.read()
+			remotefile.close()
+			for tabletype in self.ttypes:
+				# filter the entities that go by this tabletype
+				for i in JSarrayToList(contents, tabletype.clistfilter, 3):
+					# add an instance of this 
+					self.tables.append(tabletype(*[i[0],i[2],department]))
+
+	def getTable(self,id):
+		return self.getTables([id])[0]
+
+	def getTables(self,wanted):
+		""" Fetch tables """
+		# tables need to be fetched one department at a time.
+		for dept in self.departments:
+			indept = filter(lambda t: t.department == dept,wanted)
+			# fetch tables with the same tabletype together. 
+			for tabletype in map(lambda x: x.typeid, self.ttypes): 
+				# filter tables with this type from the wanted list
+				tables = filter(lambda t: t.typeid == tabletype,indept)
+				if not tables:
+					continue
+				print "debug tables=", map(lambda x: x.department,tables)
+				# generic post variables
+				d = urllib.urlencode({	"weeks":"1-13", # FIXME
+										"type": tabletype,	
+										"filter":"(None)",
+										"dept":dept})
+				# add a list of identifiers to the post request
+				d = "&".join([d]+map(lambda x: urllib.urlencode({"identifier[]": x.id}),tables))
+				# fetch
+				remotefile = urllib.urlopen(self.geturl,d)
+				source = remotefile.read()
+				remotefile.close()
+				# build tree
+				soup = BeautifulSoup(source)
+
+				x = open("".join(map(lambda x: x.name,tables)),'w')
+				x.write(soup.prettify()) #encode('utf-8'))
+				x.close()
+				# Iterate through all TIMETABLES in the HTML
+				for ttheader in soup.html.body.findAll("table",{"class":"header-border-args"}):
+					id = ttheader.find('span',{'class':'header-2-1-1'}).string.strip()
+					try:
+						x = filter(lambda x: x.name == id, tables)[0]
+					except KeyError:
+						raise ParseError, "Returned table not requested"
+					x.Parse(ttheader)
+		return wanted
+
+	def byName(self,id):
+		""" return an iterator over lectures.
+			@id the name of the klas/prof/room """
+		if not self.tables:
+			self.UpdateCandidates()
+		x = filter(lambda x: x.name == id, self.tables)
+		return x
+
 
 
 def IcalGlue(x):
