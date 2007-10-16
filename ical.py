651322c68c52ffea29959db28e37d32b138911f4
Thomas Langewouters <thomas.langewouters@thouters.be>
2007-10-16 22:19:55 +0200

    GPL License header inclusion, BeautifulSoup.py added

--- ical.py
+++ ical.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python
+#
+# Copyright 2007 Thomas Langewouters
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Ical classes
-# (c) 2007 Thomas Langewouters
-
-# http://www.ietf.org/rfc/rfc2445.txt
+# iCal implementation 
+# (see http://www.ietf.org/rfc/rfc2445.txt for reference)
 
 import datetime, socket, string
 
